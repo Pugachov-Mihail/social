d243ca2b495997e38172b0ee33f6aee2d896c471
Miha <xd383727@gmail.com>
2021-12-12 17:17:14 +0300

    create backend

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <register-form />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RegisterForm from "../components/RegisterForm.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    RegisterForm,
+  },
+};
 </script>
